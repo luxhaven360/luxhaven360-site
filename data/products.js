05a995af57172cfd869a4a33957fb1ff939debf5
Mdrean <luxhaven360@gmail.com>
2025-10-29 17:55:05 +0100

    Printful products

---
+++ data/products.js
@@ -0,0 +1,21 @@
+// data/products.js
+
+export const PRODUCTS = [
+  {
+    sku: "TSHIRT_PREMIUM",
+    name: "T-Shirt Premium",
+    description: "Collezione di abbigliamento esclusivo in cotone pettinato con logo discreto.",
+    price_cents: 12000,
+    image: "https://files.printful.com/products/mockups/tshirt-example.jpg",
+    variant_id: 1234567890 // <- ID variante Printful
+  },
+  {
+    sku: "ART_PRINT",
+    name: "Art Print",
+    description: "Stampe fotografiche in edizione limitata delle proprietà iconiche.",
+    price_cents: 25000,
+    image: "https://files.printful.com/products/mockups/artprint-example.jpg",
+    variant_id: 9876543210
+  },
+  // Aggiungi altri prodotti qui
+];
